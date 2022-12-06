16b35ec935aea1fe7ecfd2932690e491059e9250
asagal <anatole@crossvista>
2022-12-06 13:12:16 -0800

    abc

--- src/main/java/org/apache/commons/mail/Radek.java
+++ src/main/java/org/apache/commons/mail/Radek.java
@@ -1,22 +1,6 @@
 package org.apache.commons.mail;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Radek {
     private static final String PROPERTY = "abc";
 
-    public static Integer addInts(int a, int b) {
-        //add ints
-        if(PROPERTY.equalsIgnoreCase("ABC")) {
-            return a+b;
-        }
-
-        return 0;
-    }
-
-    public Radek() {
-        List<String> aaa = new ArrayList<>();
-
-    }
 }
